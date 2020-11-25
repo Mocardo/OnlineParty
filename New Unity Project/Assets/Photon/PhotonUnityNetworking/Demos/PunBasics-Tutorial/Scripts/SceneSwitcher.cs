8e36526c9e70d909a607bfe31936363601961e04
caio.ggomes <caio.graca@gmail.com>
2020-11-24 21:35:28 -0300

    inutil

--- New Unity Project/Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/SceneSwitcher.cs
+++ New Unity Project/Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/SceneSwitcher.cs
@@ -5,35 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-
     public void ToChat()
     {
-        Globals.OriginScene = SceneManager.GetActiveScene().buildIndex;
-        Debug.LogFormat("saindo da cena {0}", Globals.OriginScene);
-        int scene = SceneManager.GetSceneByName("DemoChat-Scene").buildIndex;
-        Debug.LogFormat("cena do chat a ser habilitada: {0}", scene);
-        if (Globals.Inicio)
-        {
-            SceneManager.LoadScene(5, LoadSceneMode.Additive);
-            Globals.Inicio = false;
-        }
-        else
-        {
-            foreach (GameObject g in SceneManager.GetSceneByName("DemoChat-Scene").GetRootGameObjects())
-            {
-                if(g.name != "Missing App Id Canvas")
-                {
-                    g.SetActive(true);
-                }
-            }
-        }
+        GameObject.Find("/Chat").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("Missing App Id Canvas").GetComponent<Renderer>().enabled = false;
+        GameObject.Find("/Scenery").GetComponent<Renderer>().enabled = false;
+        GameObject.Find("/EventSystem").GetComponent<Renderer>().enabled = false;
+        GameObject.Find("/Canvas").GetComponent<Renderer>().enabled = false;
+        GameObject.Find("/Main Camera").GetComponent<Renderer>().enabled = false;
+        GameObject.Find("/Game Manager").GetComponent<Renderer>().enabled = false;
     }
     public void BackToOrigin()
     {
-        Debug.LogFormat("voltando para a cena {0}", Globals.OriginScene);
-        foreach (GameObject g in SceneManager.GetSceneByName("DemoChat-Scene").GetRootGameObjects())
-        {
-            g.SetActive(false);
-        }
+        GameObject.Find("/Chat").GetComponent<Renderer>().enabled = false;
+        GameObject.Find("/Scenery").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("/EventSystem").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("/Canvas").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("/Main Camera").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("/Game Manager").GetComponent<Renderer>().enabled = true;
     }
 }
